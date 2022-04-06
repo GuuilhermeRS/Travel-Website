eb19faf8057ca301658f82afa288e1be71c1fd86
GuilhermeRS <guilherme.gke@gmail.com>
2022-04-06 09:45:17 -0300

    Adicionando Styled Component e criando estilos globais

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import Destination from './components/Destination';
+import GlobalStyles from './styles/global';
+
 import Header from './components/Header';
-import Nav from './components/Header/Nav'
 import Routes from './Routes';
 
 export default function App() {
   return (
     <div className='background'>
       <BrowserRouter>
+      <GlobalStyles />
         <Header />
         <div className="content">
           <Routes />
---
+++ src/styles/global.js
@@ -0,0 +1,83 @@
+import { createGlobalStyle } from "styled-components";
+
+const colors = {
+  light: '#FFF',
+  dark: '#0B0D17',
+  blue: '#D0D6F9',
+}
+
+const fonts = {
+  serif: `'Bellefair', serif`,
+  sansSerif: `'Barlow Condensed', sans-serif`,
+}
+
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Barlow+Condensed&family=Bellefair&display=swap');
+
+  body {
+    background: ${colors.blue};
+    background-image: url(./background-home-desktop.jpg);
+    background-size: cover;
+
+    font-family: ${fonts.sansSerif};
+    font-weight: 400;
+    color: ${colors.light};
+  }
+
+  h1 {
+    font-family: ${fonts.serif};
+    font-weight: 400;
+    font-size: 150px;
+    
+    text-transform: uppercase;
+  }
+
+  h2 {
+    font-family: ${fonts.serif};
+    font-weight: 400;
+    font-size: 100px;
+
+
+    text-transform: uppercase;
+  }
+
+  h3 {
+    font-family: ${fonts.serif};
+    font-weight: 400;
+    font-size: 56px;
+
+    text-transform: uppercase;
+  }
+
+  h4 {
+    font-family: ${fonts.serif};
+    font-weight: 400;
+    font-size: 32px;
+
+    text-transform: uppercase;
+  }
+
+  h5 {
+    font-weight: 400;
+    font-size: 28px;
+
+    letter-spacing: 4.75px;
+
+    text-transform: uppercase;
+  }
+
+  h6 {
+    font-weight: 400;
+    font-size: 28px;
+  }
+
+  p {
+    font-weight: 400;
+    font-size: 18px;
+
+    max-width: 540px;
+  }
+
+
+`
