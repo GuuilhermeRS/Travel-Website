e3f741cfaad0889c787761099473a23c982a7410
GuilhermeRS <guilherme.gke@gmail.com>
2022-04-07 09:44:04 -0300

    Animando transição entre Rotas

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
 
 import Home from './components/Home';
 import Destination from './components/Destination';
 
 export default function Routes() {
-  return(
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/destination" component={Destination} />
-    </Switch>
-  )
+  const location = useLocation();
+  const transitions = useTransition(location, {
+    from:{ opacity: 0, transform: 'translateY(50px)', position: 'absolute' },
+    enter: { opacity: 1, transform: 'translateY(0)', position: 'absolute' },
+    leave: { opacity: 0, transform: 'translateY(50px)', position: 'absolute' },
+  });
+
+  return transitions((props, item) => (
+    <animated.div style={props}>
+      <Switch location={item}>
+        <Route exact path="/" component={Home} />
+        <Route path="/destination" component={Destination} />
+      </Switch>
+    </animated.div>
+  ));
 }
 
